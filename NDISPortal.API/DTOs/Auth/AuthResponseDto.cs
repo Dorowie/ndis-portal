a245457d1c4cd932ba50cd1813e5fc870607a2cc
Dorowie <saclaojohnaxcel@gmail.com>
2026-04-23 23:51:32 +0800

    Remove Sensitive data from the responses
    Remove Sensitive data from the responses

--- NDISPortal.API/DTOs/Auth/AuthResponseDto.cs
+++ NDISPortal.API/DTOs/Auth/AuthResponseDto.cs
@@ -4,14 +4,6 @@ namespace NDISPortal.API.DTOs.Auth
 {
     public class AuthResponseDto
     {
-        [JsonPropertyName("user_id")]
-        public int UserId { get; set; }
-
-        [JsonPropertyName("email")]
-        public string Email { get; set; } = string.Empty;
-
-        [JsonPropertyName("role")]
-        public string Role { get; set; } = string.Empty;
 
         [JsonPropertyName("token")]
         public string Token { get; set; } = string.Empty;
